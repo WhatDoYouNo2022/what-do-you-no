3245e4732f3ffdf6b19f51df3d70aac3b8c7c717
itsninam <madalinaoancea@hotmail.com>
2022-05-09 17:05:00 -0400

    added api call in fetchData component, remove unused state from SlideOutNav

--- src/App.js
+++ src/App.js
@@ -1,80 +1,85 @@
 // Modules
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 // Components
-import Header from "./Components/Header.js"
-import Footer from "./Components/Footer.js"
-import LeaderboardForm from './Components/LeaderboardForm.js';
+import Header from "./Components/Header.js";
+import Footer from "./Components/Footer.js";
+import LeaderboardForm from "./Components/LeaderboardForm.js";
+import FetchData from "./Components/FetchData.js";
+
 // Utilities
-import firebase from "./utils/Firebase.js"
+import firebase from "./utils/Firebase.js";
 // Styling
-import './styles/sass/App.scss';
-import SlideOutNav from './Components/SlideOutNav.js';
+import "./styles/sass/App.scss";
+import SlideOutNav from "./Components/SlideOutNav.js";
 
 function App() {
   // state variables
   // state of whether the slide out menu is open or closed
-  const [ navOpen, setNavOpen ] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
   // state of whether the leaderboard is to be displayed
-  const [ leaderboardOpen, setLeaderboardOpen ] = useState(false);
+  const [leaderboardOpen, setLeaderboardOpen] = useState(false);
   // state of whether the about section is to be displayed.
-  const [ aboutOpen, setAboutOpen ] = useState(false);
+  const [aboutOpen, setAboutOpen] = useState(false);
 
   // Event handler
 
   // Event handler for the About navigation button
   const handleAboutButtonClick = () => {
     // changes the state of the the slide out menu depending on the current state of the menu
-    if(navOpen && leaderboardOpen){
+    if (navOpen && leaderboardOpen) {
       // if the slide out is already open showing the leaderboard, switch to the about section view
-        setLeaderboardOpen(false);
-        setAboutOpen(true);
-    } else if(!navOpen){
-      // if the slide out is closed, open the slide out menu showing the about section 
+      setLeaderboardOpen(false);
+      setAboutOpen(true);
+    } else if (!navOpen) {
+      // if the slide out is closed, open the slide out menu showing the about section
       setAboutOpen(true);
       setNavOpen(true);
       setLeaderboardOpen(false);
-    } else if(aboutOpen){
+    } else if (aboutOpen) {
       // if the about section is already open close the slide out on button click
       setNavOpen(false);
       setAboutOpen(false);
       setLeaderboardOpen(false);
     }
-  }
+  };
   // Event handler for the Leaderboard navigation button
   const handleLeaderboardClick = () => {
     // Changes the state of the slide out depending on its current state
-    if(navOpen && aboutOpen){
+    if (navOpen && aboutOpen) {
       // if the slide out is already open showing the about section, switch to the leaderboard view
-        setAboutOpen(false);
-        setLeaderboardOpen(true);
-    } else if(!navOpen){
-      // if the slide out is closed, open the slide out menu showing the leaderboard section 
+      setAboutOpen(false);
+      setLeaderboardOpen(true);
+    } else if (!navOpen) {
+      // if the slide out is closed, open the slide out menu showing the leaderboard section
       setLeaderboardOpen(true);
       setNavOpen(true);
       setAboutOpen(false);
-    } else if(leaderboardOpen){
+    } else if (leaderboardOpen) {
       // if the leaderboard section is already open, close the slide out on button click
       setNavOpen(false);
       setLeaderboardOpen(false);
       setAboutOpen(false);
     }
-  }
+  };
   return (
     <div className="App">
-      <Header 
-      handleLeaderboardClick={handleLeaderboardClick}
-      handleAboutButtonClick={handleAboutButtonClick}
-      leaderboardOpen={leaderboardOpen}
-      aboutOpen={aboutOpen}
-      />
-      <main>
-        {navOpen ? <SlideOutNav 
-        navOpen={navOpen}
-        leaderboardOpen={leaderboardOpen}
-        aboutOpen={aboutOpen}
+      <Header
         handleLeaderboardClick={handleLeaderboardClick}
         handleAboutButtonClick={handleAboutButtonClick}
-        /> : null}
+        leaderboardOpen={leaderboardOpen}
+        aboutOpen={aboutOpen}
+      />
+      <main>
+        <FetchData />
+        {navOpen ? (
+          <SlideOutNav
+            navOpen={navOpen}
+            leaderboardOpen={leaderboardOpen}
+            aboutOpen={aboutOpen}
+            handleLeaderboardClick={handleLeaderboardClick}
+            handleAboutButtonClick={handleAboutButtonClick}
+          />
+        ) : null}
         <div className="wrapper">
           <h2>What Do You No?</h2>
           <LeaderboardForm />
--- src/Components/SlideOutNav.js
+++ src/Components/SlideOutNav.js
@@ -1,27 +1,27 @@
 // Modules
-import { useState } from "react";
+
 // Components
-import Leaderboard from "./Leaderboard.js"
-import About from "./About.js"
+import Leaderboard from "./Leaderboard.js";
+import About from "./About.js";
 
 const SlideOutNav = (props) => {
-    const {
-        navOpen,
-        leaderboardOpen,
-        aboutOpen,
-        handleLeaderboardClick,
-        handleAboutButtonClick
-    } = props
+  const {
+    navOpen,
+    leaderboardOpen,
+    aboutOpen,
+    handleLeaderboardClick,
+    handleAboutButtonClick,
+  } = props;
 
-    return (
-        <aside className={navOpen ? "slideOutNav slideOutOpen" : "slideOutNav"}>
-            {leaderboardOpen ? 
-            <Leaderboard
-            handleLeaderboardClick={handleLeaderboardClick}/> : aboutOpen ?
-            <About 
-            handleAboutButtonClick={handleAboutButtonClick}/> : null}
-        </aside>
-    )
-}
+  return (
+    <aside className={navOpen ? "slideOutNav slideOutOpen" : "slideOutNav"}>
+      {leaderboardOpen ? (
+        <Leaderboard handleLeaderboardClick={handleLeaderboardClick} />
+      ) : aboutOpen ? (
+        <About handleAboutButtonClick={handleAboutButtonClick} />
+      ) : null}
+    </aside>
+  );
+};
 
 export default SlideOutNav;
