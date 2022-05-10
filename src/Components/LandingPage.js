8128248f4d55cbbabbf5fd9e6fd517fc3b798f81
itsninam <madalinaoancea@hotmail.com>
2022-05-10 12:30:26 -0400

    Styling landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -11,30 +11,37 @@ const LandingPage = (props) => {
   return (
     <div className="wrapper">
       <div className="bookCover">
-        <div className="topPages"></div>
-        <div className="sidePages"></div>
-        <h2 className="bookTitle">What Do You No?</h2>
-        <h3 className="firstEdition">First Edition</h3>
-        <h3 className="volumeOne">Volume 1</h3>
-        <div className="flexContainer">
-          <button onClick={handleAboutButtonClick}>
-            <FontAwesomeIcon icon={faChevronRight} aria-hidden="true" />
-            <p>Instructions</p>
-          </button>
-          <Link to="/gamepage">
-            <FontAwesomeIcon icon={faBookOpen} aria-hidden="true" />
-            <p>Start</p>
-          </Link>
+        <div className="pagesContainer">
+          <div className="topPages"></div>
+          <div className="sidePages"></div>
         </div>
-        <h3 className="junoPress">
-          <a
-            href="https://junocollege.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Juno College Press
-          </a>
-        </h3>
+        <div className="bookCoverContent">
+          <h2 className="bookTitle">What Do You No?</h2>
+          <h3 className="firstEdition">First Edition</h3>
+          <h3 className="volumeOne">Volume 1</h3>
+          <div className="flexContainer">
+            <button
+              className="mainMenuControls"
+              onClick={handleAboutButtonClick}
+            >
+              <FontAwesomeIcon icon={faChevronRight} aria-hidden="true" />
+              <p>Instructions</p>
+            </button>
+            <Link className="mainMenuControls" to="/gamepage">
+              <FontAwesomeIcon icon={faBookOpen} aria-hidden="true" />
+              <p>Start</p>
+            </Link>
+          </div>
+          <h3 className="junoPress">
+            <a
+              href="https://junocollege.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Juno College Press
+            </a>
+          </h3>
+        </div>
       </div>
     </div>
   );
