19a3ce69cdae222c18bc8f05b31e0f74de1e0f54
benbourgon <benjaminbourgon@gmail.com>
2022-05-12 15:38:40 -0400

    Added leaderboard username form error handling for empty and malicious usernames

--- src/Components/GameOverDisplay.js
+++ src/Components/GameOverDisplay.js
@@ -1,7 +1,13 @@
+// GameOverDisplay.js
+
+// Modules
+import { useState } from "react";
+// Components
 import LeaderboardForm from "./LeaderboardForm";
 
 const GameOverDisplay = (props) => {
-    const { scoreSubmitted, setScoreSubmitted } = props;
+    const { scoreSubmitted, setScoreSubmitted, score } = props;
+    const [usernameDeclined, setUsernameDeclined] = useState(false);
 
     const refreshPage = () => {
         window.location.reload(false);
@@ -10,9 +16,17 @@ const GameOverDisplay = (props) => {
     return (
         <>
             <p>Game Over!</p>
-            {scoreSubmitted ? 
-            <p>Your score has been submitted</p>
-            : <LeaderboardForm scoreSubmitted={scoreSubmitted} setScoreSubmitted={setScoreSubmitted}/>
+            {scoreSubmitted && usernameDeclined === false ? 
+                <p>Your score has been submitted</p>
+            : usernameDeclined ?
+                <p>Ok, we won't submit your score. Thanks for playing!</p>
+            :
+            <LeaderboardForm 
+                scoreSubmitted={scoreSubmitted} 
+                setScoreSubmitted={setScoreSubmitted}
+                score={score}
+                setUsernameDeclined={setUsernameDeclined}
+            />
             }
             <button onClick={refreshPage}>Restart Game</button>
         </>
