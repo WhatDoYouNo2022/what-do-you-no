bb4f88a337efae33ed65a4be4ba078d71599eba2
benbourgon <benjaminbourgon@gmail.com>
2022-05-13 12:38:45 -0400

    Added confetti animation

--- src/Components/GameOverDisplay.js
+++ src/Components/GameOverDisplay.js
@@ -2,19 +2,29 @@
 
 // Modules
 import { useState } from "react";
+// react confetti found here: https://www.npmjs.com/package/react-confetti
+import Confetti from "react-confetti"
 // Components
 import LeaderboardForm from "./LeaderboardForm";
+import useWindowSize from "../Hooks/useWindowSize";
 
 const GameOverDisplay = (props) => {
     const { scoreSubmitted, setScoreSubmitted, score } = props;
     const [usernameDeclined, setUsernameDeclined] = useState(false);
+    const { width, height } = useWindowSize();
 
     const refreshPage = () => {
         window.location.reload(false);
     }
-
+    const confettiColors = ['#6056f9', '#fff', '#cfff31']
     return (
         <>
+            <Confetti
+                width={width}
+                height={height}
+                colors={confettiColors}
+                numberOfPieces={200}
+            />
             <p>Game Over!</p>
             {scoreSubmitted && usernameDeclined === false ? 
                 <p>Your score has been submitted</p>
@@ -33,4 +43,4 @@ const GameOverDisplay = (props) => {
     )
 }
 
-export default GameOverDisplay
+export default GameOverDisplay;
---
+++ src/Hooks/useWindowSize.js
@@ -0,0 +1,34 @@
+// useWindowSize.js
+
+import { useState, useEffect } from "react";
+
+// useWindowSize hook from: https://usehooks.com/useWindowSize/
+const useWindowSize = () => {
+    // Create a state variable to hold the width and height of the window object.
+    const [ windowSize, setWindowSize ] = useState({
+        width: undefined,
+        height: undefined
+    });
+
+    useEffect(() => {
+        // Event handler to call on window resize
+        const handleWindowResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+        }
+        // Event listener for when the window size changes
+        window.addEventListener("resize", handleWindowResize);
+
+        // Get the initial size of the window right away
+        handleWindowResize();
+
+        // Cleanup function to remove the event listener
+        return () => window.removeEventListener("resize", handleWindowResize);
+    }, [])
+
+    return windowSize;
+}
+
+export default useWindowSize;
