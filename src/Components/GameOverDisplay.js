a1b279d91c8b5ade1e539f69e0c429bc7c33646b
itsninam <madalinaoancea@hotmail.com>
2022-05-14 12:53:25 -0400

    completed routing from game-over page back to landing page, small change to refresh questions

--- src/Components/GameOverDisplay.js
+++ src/Components/GameOverDisplay.js
@@ -3,44 +3,39 @@
 // Modules
 import { useState } from "react";
 // react confetti module sourced from: https://www.npmjs.com/package/react-confetti
-import Confetti from "react-confetti"
+import Confetti from "react-confetti";
+import { Link } from "react-router-dom";
+
 // Components
 import LeaderboardForm from "./LeaderboardForm";
 import useWindowSize from "../Hooks/useWindowSize";
 
 const GameOverDisplay = (props) => {
-    const { scoreSubmitted, setScoreSubmitted, score } = props;
-    const [usernameDeclined, setUsernameDeclined] = useState(false);
-    const { width, height } = useWindowSize();
-    // An array to hold the colors for the confetti.
-    const confettiColors = ['#6056f9', '#fff', '#cfff31']
-    const refreshPage = () => {
-        window.location.reload(false);
-    }
-    
-    return (
-        <>
-            <Confetti
-                width={width}
-                height={height}
-                colors={confettiColors}
-            />
-            <h2>Thanks for playing!</h2>
-            {scoreSubmitted && usernameDeclined === false ? 
-                <p>Your score has been submitted</p>
-            : usernameDeclined ?
-                <p>Ok, we won't submit your score. Thanks for playing!</p>
-            :
-            <LeaderboardForm 
-                scoreSubmitted={scoreSubmitted} 
-                setScoreSubmitted={setScoreSubmitted}
-                score={score}
-                setUsernameDeclined={setUsernameDeclined}
-            />
-            }
-            <button onClick={refreshPage}>Restart Game</button>
-        </>
-    )
-}
+  const { scoreSubmitted, setScoreSubmitted, score } = props;
+  const [usernameDeclined, setUsernameDeclined] = useState(false);
+  const { width, height } = useWindowSize();
+  // An array to hold the colors for the confetti.
+  const confettiColors = ["#6056f9", "#fff", "#cfff31"];
+
+  return (
+    <>
+      <Confetti width={width} height={height} colors={confettiColors} />
+      <h2>Thanks for playing!</h2>
+      {scoreSubmitted && usernameDeclined === false ? (
+        <p>Your score has been submitted</p>
+      ) : usernameDeclined ? (
+        <p>Ok, we won't submit your score. Thanks for playing!</p>
+      ) : (
+        <LeaderboardForm
+          scoreSubmitted={scoreSubmitted}
+          setScoreSubmitted={setScoreSubmitted}
+          score={score}
+          setUsernameDeclined={setUsernameDeclined}
+        />
+      )}
+      <Link to="/">Restart Game</Link>
+    </>
+  );
+};
 
 export default GameOverDisplay;
